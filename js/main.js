77c9bd184a3f0ea478d9a4fbcd2dc1c7e9b7bbd5
mljustinli <ml.justin.li@gmail.com>
2017-06-28 13:32:07 -0400

    Add array of positions of line

--- js/main.js
+++ js/main.js
@@ -16,11 +16,13 @@ var mouseY = 0;
 var lastPos = {x: 0, y: 0};
 var pos = {x: 0, y: 0};
 
+var positions = [];
+
 /*
 HTML Setup Stuff
 */
 
-var canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
@@ -29,22 +31,24 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 canvas.addEventListener("mousemove", function (e) {
-    console.log("mouse move");
-    console.log(e.clientX + " " + e.clientY);
+    // console.log("mouse move");
+    // console.log(e.clientX + " " + e.clientY);
     pos = {x: e.clientX, y: e.clientY};
 
 }, false);
 canvas.addEventListener("mousedown", function (e) {
-    console.log("mouse down");
+    // console.log("mouse down");
+    clearScreen();
     lastPos = {x: e.clientX, y: e.clientY};
+    positions.push(math.complex(e.clientX, e.clientY));
     mouseDown = true;
 }, false);
 canvas.addEventListener("mouseup", function (e) {
-    console.log("mouse up");
+    // console.log("mouse up");
     mouseDown = false;
 }, false);
 canvas.addEventListener("mouseout", function (e) {
-    console.log("mouse out");
+    // console.log("mouse out");
 }, false);
 
 /*
@@ -60,8 +64,17 @@ function keyDownHandler(e) {
         leftPressed = true;
     }
 
+    //67 is c
     if (e.keyCode == 67) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        clearScreen();
+    }
+
+    //68 is d
+    if (e.keyCode == 68) {
+        for (var i = 0; i < positions.length; i++) {
+            console.log(positions[i]);
+        }
+        console.log("_________________________________");
     }
 }
 
@@ -82,12 +95,18 @@ function Update() {
         ctx.moveTo(lastPos.x, lastPos.y);
         ctx.lineTo(pos.x, pos.y);
         // ctx.strokeStyle = x;
-        ctx.lineWidth = 10;
+        ctx.lineWidth = 5;
         ctx.stroke();
         ctx.closePath();
 
+        positions.push(math.complex(pos.x, pos.y));
         lastPos = {x: pos.x, y: pos.y};
     }
+}
+
+function clearScreen() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    positions = [];
 }
 
 setInterval(Update, 1000/FPS);
