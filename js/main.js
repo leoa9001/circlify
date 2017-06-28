69942acd95e4bb8d848a2a31e3ec12039d0c8a10
mljustinli <ml.justin.li@gmail.com>
2017-06-28 13:03:41 -0400

    Can draw things :O

--- js/main.js
+++ js/main.js
@@ -7,6 +7,16 @@ var WIDTH = 640;
 var HEIGHT = 480;
 
 /*
+Actual Game Variables
+*/
+
+var mouseDown = false;
+var mouseX = 0;
+var mouseY = 0;
+var lastPos = {x: 0, y: 0};
+var pos = {x: 0, y: 0};
+
+/*
 HTML Setup Stuff
 */
 
@@ -20,24 +30,22 @@ document.addEventListener("keyup", keyUpHandler, false);
 
 canvas.addEventListener("mousemove", function (e) {
     console.log("mouse move");
+    console.log(e.clientX + " " + e.clientY);
+    pos = {x: e.clientX, y: e.clientY};
+
 }, false);
 canvas.addEventListener("mousedown", function (e) {
     console.log("mouse down");
+    lastPos = {x: e.clientX, y: e.clientY};
+    mouseDown = true;
 }, false);
 canvas.addEventListener("mouseup", function (e) {
     console.log("mouse up");
+    mouseDown = false;
 }, false);
 canvas.addEventListener("mouseout", function (e) {
     console.log("mouse out");
 }, false);
-
-/*
-Actual Game Variables
-*/
-
-var mouseDown = false;
-var mouseX = 0;
-var mouseY = 0;
 
 /*
 Functions, but should be more general management functions and 
@@ -51,6 +59,10 @@ function keyDownHandler(e) {
     else if (e.keyCode == 37) {
         leftPressed = true;
     }
+
+    if (e.keyCode == 67) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
 }
 
 function keyUpHandler(e) {
@@ -63,7 +75,19 @@ function keyUpHandler(e) {
 }
 
 function Update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (mouseDown) {
+        ctx.beginPath();
+        ctx.moveTo(lastPos.x, lastPos.y);
+        ctx.lineTo(pos.x, pos.y);
+        // ctx.strokeStyle = x;
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.closePath();
+
+        lastPos = {x: pos.x, y: pos.y};
+    }
 }
 
 setInterval(Update, 1000/FPS);
